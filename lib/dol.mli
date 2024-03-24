c0edf07d4275a2e36cf1e08653988de734d46755
Nathan Farlow <natethegreatest11@gmail.com>
2024-03-24 18:26:23 -0400

    asdf

---
+++ lib/dol.mli
@@ -0,0 +1,18 @@
+val num_text_sections : int
+val num_data_sections : int
+
+type section =
+  { address : int
+  ; contents : string
+  }
+
+type t =
+  { text : section list
+  ; data : section list
+  ; bss_address : int
+  ; bss_size : int
+  ; entry_point : int
+  }
+
+val load : string -> t
+val dump : t -> string

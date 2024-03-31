7b7989040c539026fc13cc2974578cb5141eee32
Nathan Farlow <natethegreatest11@gmail.com>
2024-03-31 11:57:43 -0400

    YEAH

--- mod/main.c
+++ mod/main.c
@@ -18,24 +18,55 @@ void (*CheatLevelLocksmith)() = (void *)0x80091f5c;
  */
 void game_loop_hook() {
   CheatLevelLocksmith();
-  iter_npcs(freeze_npc);
+  /* iter_npcs(freeze_npc); */
 #ifdef HEAD_MOD
   scale_head();
 #endif
 }
 
+/* #define dbgf(a, ...) \ */
+/*   { \ */
+/*     char buf[0x100]; \ */
+/*     _sprintf(buf, a, __VA_ARGS__); \ */
+/*     draw_text(buf, 0.5, 0.5, 1.0f, 0xFF00FFFF, 0); \ */
+/*   } */
+
 xVec3 *(*xEntGetPos)(zNPCCommon *) = (void *)0x80025424;
 
-bool did = 0;
-void draw_hi(zNPCInstance *a, zNPCCommon *npc) {
-  if (did)
-    return;
-  xVec3 *v = xEntGetPos(npc);
-  dbgf("%f %f %f\n", v->x, v->y, v->z);
-  did = 1;
+#define xCamScreen void
+xCamScreen **x_cam_screen_instance = (void *)0x8039831c;
+float (*world_to_screen)(xCamScreen *, xVec3 *, xVec3 *) = (void *)0x8000d570;
+
+typedef float mat4[4][4];
+
+// Global::globals
+// has type xCamGroup*
+// position is x + 0x30
+mat4 **cam = (void *)0x80398318;
+
+/* float dist(xVec3 *a, xVec3 *b) { */
+/*   return sqrtf((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y)
+ * + */
+/*                (a->z - b->z) * (a->z - b->z)); */
+/* } */
+
+float (*_sqrtf)(float) = (void *)0x800782a8;
+
+mat4 **cam_screen = (void *)0x8039831c;
+
+float dist(xVec3 *a, xVec3 *b) {
+  return _sqrtf((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y) +
+                (a->z - b->z) * (a->z - b->z));
 }
 
-void end_of_render() {
-  did = 0;
-  iter_npcs(draw_hi);
+void draw_marker(zNPCInstance *a, zNPCCommon *npc) {
+  xVec3 *v = xEntGetPos(npc);
+  xVec3 screen;
+  float f = world_to_screen(*x_cam_screen_instance, &screen, v);
+  if (f < 0 || f > 1)
+    return;
+
+  draw_text("X", screen.x, screen.y, 0.5f, 0xFF00FFFF, 0);
 }
+
+void end_of_render() { iter_npcs(draw_marker); }

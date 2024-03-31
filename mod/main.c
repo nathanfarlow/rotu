a5a8baa58d1cb44a1af1164b7a51b791e21d5054
Nathan Farlow <natethegreatest11@gmail.com>
2024-03-30 22:47:14 -0400

    p

--- mod/main.c
+++ mod/main.c
@@ -44,64 +44,19 @@ void (*SetChase)(zNPCInstance *npc_instance, char b) = (void *)0x800cf158;
 void (*SetAttack)(zNPCInstance *npc_instance, char b) = (void *)0x800cf150;
 u8 (*get_flags)(zNPCInstance *npc_instance) = (void *)0x800aca0c;
 
-void iter_npcs(zNPCMgr *this) {
+#define NUM_NPC_TYPES 12
 
-  // 0xc is all
-  for (u32 npc_type = 1; npc_type < 2; npc_type = npc_type + 1) {
+void iter_npcs(zNPCMgr *this, void (*func)(zNPCInstance *, zNPCCommon *)) {
+  for (u32 npc_type = 1; npc_type < NUM_NPC_TYPES; npc_type = npc_type + 1) {
     for (int i = 0; i < *(int *)(this + npc_type * 4 + 0x1d0); i++) {
-      zNPCInstance *npc_instance = GetInstance(this, npc_type, i);
-      zNPCCommon *common = get_data(npc_instance);
-      if (!should_have_data(npc_instance) || common == NULL)
+      zNPCInstance *instance = GetInstance(this, npc_type, i);
+      zNPCCommon *common = get_data(instance);
+      if (!should_have_data(instance) || common == NULL)
         continue;
-      // this works
-      void (*kill)(zNPCCommon *) =
-          (void *)*(int *)(*(int *)(common + 0xdc) + 0x54);
-      dbgf("kill: %p", kill);
-      kill(common);
-      return;
+      func(instance, common);
     }
   }
 }
-
-/* void iter_npcs(zNPCMgr *this) { */
-/*   if (this == NULL) */
-/*     return; */
-
-/*   // npc_type < 0xc */
-/*   // 0 is enforcer, 1 is drill */
-/*   for (u32 npc_type = 3; npc_type < 0xc; npc_type = npc_type + 1) { */
-/*     // Length check for current npc type */
-/*     for (int i = 0; i < *(int *)(this + npc_type * 4 + 0x170); i++) { */
-/*       zNPCBase *npc_base = GetData(this, npc_type, i); */
-/*       zNPCInstance *npc_instance = get_instance(npc_base); */
-/*       u32 flags = get_flags(npc_instance); */
-
-/*       /\* void *vtable = *((void **)npc_base + 0xdc); *\/ */
-
-/*       if ((flags >> 6) & 1) { */
-/*         dbgf("type: %d, index: %d, flags: %p", npc_type, i, flags); */
-/*         SetChase(npc_instance, 1); */
-/*         SetAttack(npc_instance, 1); */
-/*       } */
-
-/*       /\* if (vtable == (void *)0x8030b500) { *\/ */
-/*       /\* dbgf("type: %d, index: %d, vtable: %p", npc_type, i, vtable); *\/
- */
-/*       /\* dbgf("type: %d, index: %d, flags: %p", npc_type, i, flags); *\/ */
-/*       /\* return; *\/ */
-/*       /\* } *\/ */
-/*       /\* void (*kill)(zNPCInstance *npc_instance) = (void *)(vtable + 84);
- * *\/ */
-/*       /\* kill(npc_instance); *\/ */
-
-/*       // might be orientation? */
-/*       /\* xVec3 *pos = (xVec3 *)((char *)npc_instance + 0x30); *\/ */
-
-/*       /\* SetChase(npc_instance, 1); *\/ */
-/*       /\* SetAttack(npc_instance, 1); *\/ */
-/*     } */
-/*   } */
-/* } */
 
 #define npc_mgr_instance ((zNPCMgr **)0x803ed9e8)
 
@@ -111,6 +66,11 @@ void game_loop_hook() {
 #ifdef HEAD_MOD
   scale_head();
 #endif
+}
+
+void kill_npc(zNPCInstance *a, zNPCCommon *npc) {
+  void (*kill)(zNPCCommon *) = (void *)*(int *)(*(int *)(npc + 0xdc) + 0x54);
+  kill(npc);
 }
 
 void end_of_render() {
@@ -123,5 +83,5 @@ void end_of_render() {
 
   /* draw_text("yeet", x, y, 4.0f, (color << 24 | 0x00FFFF), (color << 16 |
    * 0xff)); */
-  iter_npcs(*npc_mgr_instance);
+  iter_npcs(*npc_mgr_instance, kill_npc);
 }

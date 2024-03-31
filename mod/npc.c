922dc5c8b812837a0a6785c7580df3e198ed48c8
Nathan Farlow <natethegreatest11@gmail.com>
2024-03-30 22:59:27 -0400

    asdf

--- mod/npc.c
+++ mod/npc.c
@@ -19,13 +19,15 @@ void (*SetAttack)(zNPCInstance *, char b) = (void *)0x800cf150;
 zNPCMgr **npc_mgr_instance = (void *)0x803ed9e8;
 
 void iter_npcs(void (*f)(zNPCInstance *, zNPCCommon *)) {
-  for (u32 npc_type = 0; npc_type < NUM_NPC_TYPES; npc_type = npc_type + 1) {
+  for (u32 npc_type = 0; npc_type < NUM_NPC_TYPES; npc_type++) {
     for (int i = 0; i < *(int *)(*npc_mgr_instance + npc_type * 4 + 0x1d0);
          i++) {
       zNPCInstance *instance = GetInstance(*npc_mgr_instance, npc_type, i);
       zNPCCommon *common = GetData(instance);
+
       if (!ShouldHaveData(instance) || common == NULL)
         continue;
+
       f(instance, common);
     }
   }

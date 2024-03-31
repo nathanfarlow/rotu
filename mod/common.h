63d0c83c225ff10f8b6e3232bdd3edad5cef2b06
Nathan Farlow <natethegreatest11@gmail.com>
2024-03-30 22:58:08 -0400

    foo

--- mod/common.h
+++ mod/common.h
@@ -2,7 +2,8 @@
 
 #define NULL 0
 #define u32 unsigned int
-#define bool unsigned char
+#define u8 unsigned char
+#define bool u8
 
 #define sinf ((float (*)(float))0x8007d7f0)
 #define cosf ((float (*)(float))0x8007d834)
@@ -18,3 +19,9 @@
     _sprintf(buf, a, __VA_ARGS__);                                             \
     draw_text(buf, 0.5, 0.5, 1.0f, 0xFF00FFFF, 0);                             \
   }
+
+typedef struct xVec3 {
+  float x;
+  float y;
+  float z;
+} xVec3;
--- mod/npc.c
+++ mod/npc.c
@@ -2,43 +2,29 @@
 #include "common.h"
 
 #define zNPCMgr void
-#define zNPCBase void
 #define zNPCInstance void
 #define zNPCCommon void
 
-zNPCBase *(*GetData)(zNPCMgr *this, int npc_type,
-                     int index) = (void *)0x800d0ed0;
-
-zNPCInstance *(*GetInstance)(zNPCBase *npc_base, int npc_type,
+zNPCInstance *(*GetInstance)(zNPCMgr *npc_mgr, int npc_type,
                              int index) = (void *)0x800d0ea4;
 
-zNPCCommon *(*get_data)(zNPCInstance *npc_instance) = (void *)0x80091b3c;
-bool (*should_have_data)(zNPCInstance *npc_instance) = (void *)0x801b94c8;
+bool (*ShouldHaveData)(zNPCInstance *) = (void *)0x801b94c8;
+zNPCCommon *(*GetData)(zNPCInstance *) = (void *)0x80091b3c;
 
-typedef struct xVec3 {
-  float x;
-  float y;
-  float z;
-} xVec3;
-
-xVec3 *(*get_last_pos)(zNPCInstance *npc_instance) = (void *)0x800bd09c;
-
-typedef unsigned char u8;
-void (*SetChase)(zNPCInstance *npc_instance, char b) = (void *)0x800cf158;
-void (*SetAttack)(zNPCInstance *npc_instance, char b) = (void *)0x800cf150;
-u8 (*get_flags)(zNPCInstance *npc_instance) = (void *)0x800aca0c;
+void (*SetChase)(zNPCInstance *, char b) = (void *)0x800cf158;
+void (*SetAttack)(zNPCInstance *, char b) = (void *)0x800cf150;
 
 #define NUM_NPC_TYPES 12
 
-#define npc_mgr_instance ((zNPCMgr **)0x803ed9e8)
+zNPCMgr **npc_mgr_instance = (void *)0x803ed9e8;
 
 void iter_npcs(void (*f)(zNPCInstance *, zNPCCommon *)) {
   for (u32 npc_type = 0; npc_type < NUM_NPC_TYPES; npc_type = npc_type + 1) {
     for (int i = 0; i < *(int *)(*npc_mgr_instance + npc_type * 4 + 0x1d0);
          i++) {
       zNPCInstance *instance = GetInstance(*npc_mgr_instance, npc_type, i);
-      zNPCCommon *common = get_data(instance);
-      if (!should_have_data(instance) || common == NULL)
+      zNPCCommon *common = GetData(instance);
+      if (!ShouldHaveData(instance) || common == NULL)
         continue;
       f(instance, common);
     }
